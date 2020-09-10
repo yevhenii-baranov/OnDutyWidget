44a98cb111793d3f6f248cef6a595c72d2e89dc1
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2020-09-10 13:49:43 +0300

    Use older Java API for Android 6.0

--- app/src/main/java/io/github/effffgen/onduty/service/OnDutyService.kt
+++ app/src/main/java/io/github/effffgen/onduty/service/OnDutyService.kt
@@ -1,18 +1,21 @@
 package io.github.effffgen.onduty.service
 
 import io.github.effffgen.onduty.Entity
-import java.time.LocalDate
-import java.time.temporal.ChronoUnit
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-val initialDate: LocalDate = LocalDate.of(2019, 11, 4)
+val year = 2019
+val month = Calendar.NOVEMBER
+val day = 4
 
 class OnDutyService {
     fun whoIsOnDutyToday(): Entity {
-        val currentDate = LocalDate.now()
-        val daysFromStart = ChronoUnit.DAYS.between(initialDate, currentDate)
+        val initialDate = Calendar.getInstance()
+        initialDate.set(year, month, day, 0, 0, 0)
 
-        println(daysFromStart)
-        println(daysFromStart % 3)
+        val currentDate = Calendar.getInstance()
+
+        val daysFromStart = TimeUnit.DAYS.convert(currentDate.timeInMillis - initialDate.timeInMillis, TimeUnit.MILLISECONDS)
 
         return when (daysFromStart % 3) {
             0L -> Entity.ICHI
